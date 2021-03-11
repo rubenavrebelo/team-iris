9261e2dcafa7bef239cbce78377be4c0b3a82709
Rúben <rav.rebelo@campus.fct.unl.pt>
2021-03-11 18:01:58 +0000

    Types & Page Demo.

---
+++ src/components/demo/PageDemo.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import StreamerSection from '../../sections/streamers/StreamersSection';
+import TextSection from '../../sections/text-section/TextSection';
+import Navbar from '../navbar/Navbar';
+import grey from '@material-ui/core/colors/grey';
+import { Typography } from '@material-ui/core';
+
+const footbarColor = grey[900];
+
+
+export default function PageDemo() {
+
+    return (
+        <div id={'main'}>
+            <Navbar />
+            <StreamerSection />
+            <TextSection sectionTitle={'Test'} />
+            <TextSection sectionTitle={'On the Right'} alignment={'right'}/>
+            <TextSection sectionTitle={'On the Center'} alignment={'center'}/>
+            <footer style={{height: 100, width: '100%', backgroundColor: footbarColor}}>
+                <Typography style={{color: 'white', marginTop: 50}}>Made with love</Typography>
+            </footer>
+        </div>
+    );
+}
---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export interface StreamerObject {
+    twitchName: string;
+    description: string;
+    url: string;
+    avatar: string;
+} 
+
+
+// Dummy Streamer Object for testing
+export const Eevo : StreamerObject = {
+    twitchName: 'Eevolicious',
+    description: `Your favorite Portuguese chaotic magical girl who's sole purpose is to be stanned by you. Her favorite color is "homophonic tears" and her favorite wig is a broccoli. If you just found out about the channel, you're late, hurry up and follow.`,
+    url: 'https://www.twitch.tv/eevolicious',
+    avatar: 'https://static-cdn.jtvnw.net/jtv_user_pictures/63e1b530-642c-462a-b34f-e048b7469975-profile_image-300x300.png'
+}
